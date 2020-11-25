862ffcca388afcc85a31fafb6f7d2a5096769d29
StrangeGeorge81 <pandis.g8@gmail.com>
2020-11-25 20:32:07 +0200

    ade gamisou, kanei kuklous me pseudofade

--- spindicator_ws2812b/spindicator_ws2812b.ino
+++ spindicator_ws2812b/spindicator_ws2812b.ino
@@ -5,31 +5,80 @@
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
+/*      
+*       pixels.setPixelColor(n, red, green, blue);
+*       n = NUMPIXELS <=> nas (0 to 11)
+*       rgb stated from 0 to 100
+*/
+
 void setup() {
+    
     pixels.begin();
-    // pixels.clear();
-    // pixels.show();
+
 }
 
 
 void loop() {
-    uint32_t colour = pixels.Color(255, 0, 0);
-    int initial_bright = 0;
-    int last_bright = 255;
-    int step = 1;
-    int delay_ = 1;
-    
-    for (int i=initial_bright; i<=last_bright; i=i+step){
-        pixels.fill(colour);
-        pixels.setBrightness(i);
-        pixels.show();
-        delay(delay_);
-    }
-    for (int i=last_bright; i>=initial_bright; i=i-step){
-        pixels.fill(colour);
-        pixels.setBrightness(i);
-        pixels.show();
-        delay(delay_);
+
+    for (int nas=11; nas>=0; nas--){
+
+        if (nas==11){
+
+            pixels.setBrightness(80);
+            pixels.setPixelColor (nas, 100, 64, 255);
+            pixels.show();
+            delay(11);
+            pixels.setBrightness(40);
+            pixels.setPixelColor (0, 100, 64, 255);
+            pixels.show();
+            delay(11);
+            pixels.setBrightness(1);
+            pixels.setPixelColor (1, 100, 255, 64);
+            pixels.show();
+            delay(11);
+            pixels.clear();
+            delay (68);
+
+        }
+
+        if (nas==10){
+            
+            pixels.setBrightness(80);
+            pixels.setPixelColor (nas, 100, 64, 255);
+            pixels.show();
+            delay(11);
+            pixels.setBrightness(40);
+            pixels.setPixelColor (11, 100, 64, 255);
+            pixels.show();
+            delay(11);
+            pixels.setBrightness(1);
+            pixels.setPixelColor (0, 100, 255, 64);
+            pixels.show();
+            delay(11);
+            pixels.clear();
+            delay (68);
+
+        }
+
+        if (nas!=11 and nas!=10){
+
+            pixels.setBrightness(80);
+            pixels.setPixelColor (nas, 100, 64, 255);
+            pixels.show();
+            delay(11);
+            pixels.setBrightness(40);
+            pixels.setPixelColor (nas+1, 100, 64, 255);
+            pixels.show();
+            delay(11);
+            pixels.setBrightness(1);
+            pixels.setPixelColor (nas+2, 100, 255, 64);
+            pixels.show();
+            delay(11);
+            pixels.clear();
+            delay (68);
+
+        }
+
     }
 
 }
