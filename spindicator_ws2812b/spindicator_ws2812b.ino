bafdfef07e0383d80a44bf0e53d1c8f302195e9a
StrangeGeorge81 <pandis.g8@gmail.com>
2020-11-25 21:09:08 +0200

    na doume pws tha paei me func+arrays

--- spindicator_ws2812b/spindicator_ws2812b.ino
+++ spindicator_ws2812b/spindicator_ws2812b.ino
@@ -7,9 +7,72 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 /*      
 *       pixels.setPixelColor(n, red, green, blue);
-*       n = NUMPIXELS <=> nas (0 to 11)
-*       rgb stated from 0 to 100
+*       NUMPIXELS <=> nas 
+*       rd/gr/bl/brght are stated from 0 to 255
+*
 */
+int nas = 11;
+int brght = pinax;
+int funk = brght/4;
+int rd = pinax;
+int gr = pinax;
+int bl = pinax;
+void circlex3left (){
+
+    for (nas=11; nas>=0; nas--){
+
+        if (nas==11){
+
+            pixels.setBrightness(brght);
+            pixels.setPixelColor (nas, rd, gr, bl);
+            pixels.show();
+            pixels.setBrightness(funk);
+            pixels.setPixelColor (0, rd, gr, bl);
+            pixels.show();
+            pixels.setBrightness();
+            pixels.setPixelColor (1, rd, gr, bl);
+            pixels.show();
+            pixels.clear();
+            delay (281);
+
+        }
+
+        if (nas==10){
+            
+            pixels.setBrightness(brght);
+            pixels.setPixelColor (nas, rd, gr, bl);
+            pixels.show();
+            pixels.setBrightness(funk);
+            pixels.setPixelColor (11, rd, gr, bl);
+            pixels.show();
+            pixels.setBrightness(1);
+            pixels.setPixelColor (0, rd, gr, bl);
+            pixels.show();
+            pixels.clear();
+            delay (281);
+
+        }
+
+        if (nas!=11 and nas!=10){
+
+            pixels.setBrightness(brght);
+            pixels.setPixelColor (nas, rd, gr, bl);
+            pixels.show();
+            pixels.setBrightness(funk);
+            pixels.setPixelColor (nas+1, rd, gr, bl);
+            pixels.show();
+            pixels.setBrightness(1);
+            pixels.setPixelColor (nas+2, rd, gr, bl);
+            pixels.show();
+            pixels.clear();
+            delay (281);
+
+        }
+
+    }
+    
+}
+
 
 void setup() {
     
@@ -20,65 +83,5 @@ void setup() {
 
 void loop() {
 
-    for (int nas=11; nas>=0; nas--){
-
-        if (nas==11){
-
-            pixels.setBrightness(80);
-            pixels.setPixelColor (nas, 100, 64, 255);
-            pixels.show();
-            delay(11);
-            pixels.setBrightness(40);
-            pixels.setPixelColor (0, 100, 64, 255);
-            pixels.show();
-            delay(11);
-            pixels.setBrightness(1);
-            pixels.setPixelColor (1, 100, 255, 64);
-            pixels.show();
-            delay(11);
-            pixels.clear();
-            delay (68);
-
-        }
-
-        if (nas==10){
-            
-            pixels.setBrightness(80);
-            pixels.setPixelColor (nas, 100, 64, 255);
-            pixels.show();
-            delay(11);
-            pixels.setBrightness(40);
-            pixels.setPixelColor (11, 100, 64, 255);
-            pixels.show();
-            delay(11);
-            pixels.setBrightness(1);
-            pixels.setPixelColor (0, 100, 255, 64);
-            pixels.show();
-            delay(11);
-            pixels.clear();
-            delay (68);
-
-        }
-
-        if (nas!=11 and nas!=10){
-
-            pixels.setBrightness(80);
-            pixels.setPixelColor (nas, 100, 64, 255);
-            pixels.show();
-            delay(11);
-            pixels.setBrightness(40);
-            pixels.setPixelColor (nas+1, 100, 64, 255);
-            pixels.show();
-            delay(11);
-            pixels.setBrightness(1);
-            pixels.setPixelColor (nas+2, 100, 255, 64);
-            pixels.show();
-            delay(11);
-            pixels.clear();
-            delay (68);
-
-        }
-
-    }
 
 }
