7f68f7463d5200dd1cf4119416bcc1299dc7aa28
StrangeGeorge81 <pandis.g8@gmail.com>
2020-12-14 15:49:51 +0200

    double button to hue change

--- spindicator_ws2812b/spindicator_ws2812b.ino
+++ spindicator_ws2812b/spindicator_ws2812b.ino
@@ -5,106 +5,94 @@
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-/*      
-*       pixels.setPixelColor(n, red, green, blue);
-*       NUMPIXELS <=> nas 
-*       rd/gr/bl/brght are stated from 0 to 255
-*
-*/
 int nas = 11;
-int brght = 0;
-int funk = brght/4;
-int rd = 0;
-int gr = 0;
-int bl = 0;
-int delay_ = 53;
-bool btn = 0;
 
-uint32_t rgbcolor = pixels.ColorHSV(54613, 255, brght);
+int brght = 0; //initial goes up to 48 (not higher, too bright!!)
+
+int funk = 0;
+// funk apo 0 to 65535
+
+int delay_ = 42; //static
+
+bool btn = 0;
+bool btn2 = 0;
+
+uint32_t rgbcolor = pixels.ColorHSV(funk, 255, brght);
+
+void HSV_around(int a, int b){
+        
+    rgbcolor = pixels.ColorHSV(funk, 255, brght);
+    pixels.setPixelColor (nas, rgbcolor);
+
+    rgbcolor = pixels.ColorHSV(funk, 250, brght/2);
+    pixels.setPixelColor (a, rgbcolor);
+
+    rgbcolor = pixels.ColorHSV(funk, 240, brght/16);
+    pixels.setPixelColor (b, rgbcolor);
+
+    pixels.show();
+
+}
 
 void circlex3left (){
 
-    // for (nas=11; nas>=0; nas--){
+    if (nas==11){
 
-        if (nas==11){
+        HSV_around(0, 1);
 
-            rgbcolor = pixels.ColorHSV(54613, 255, brght);
-            pixels.setPixelColor (nas, rgbcolor);
+    }
 
-            rgbcolor = pixels.ColorHSV(54613, 255, brght/2);
-            pixels.setPixelColor (0, rgbcolor);
+    if (nas==10){
+        
+        HSV_around(11, 0);
 
-            rgbcolor = pixels.ColorHSV(54613, 255, brght/8);
-            pixels.setPixelColor (1, rgbcolor);
+    }
 
-            pixels.show();
+    if (nas!=11 && nas!=10){
 
-        }
+       HSV_around(nas+1, nas+2);
 
-        if (nas==10){
-            
-            rgbcolor = pixels.ColorHSV(54613, 255, brght);
-            pixels.setPixelColor (nas, rgbcolor);
-            
-            rgbcolor = pixels.ColorHSV(54613, 255, brght/2);
-            pixels.setPixelColor (11, rgbcolor);
-            
-            rgbcolor = pixels.ColorHSV(54613, 255, brght/8);
-            pixels.setPixelColor (0, rgbcolor);
+    }
 
-            pixels.show();
-
-        }
-
-        if (nas!=11 && nas!=10){
-
-            rgbcolor = pixels.ColorHSV(54613, 255, brght);
-            pixels.setPixelColor (nas, rgbcolor);
-            
-            rgbcolor = pixels.ColorHSV(54613, 255, brght/2);
-            pixels.setPixelColor (nas+1, rgbcolor);
-            
-            rgbcolor = pixels.ColorHSV(54613, 255, brght/8);
-            pixels.setPixelColor (nas+2, rgbcolor);
-
-            pixels.show();
-
-        }
-
-
-        delay (delay_);
-        pixels.clear();
-
-    //}
+    delay (delay_);
+    pixels.clear();
     
 }
 
-// byte bright = 0;
-// uint32_t rgbcolor = pixels.ColorHSV(0, 0, bright);
+void funkyColors (){
 
-// while(1){
-//     pixels.fill(rgbcolor);
-//     pixels.show();
-//     bright += 1;
-//     rgbcolor = pixels.ColorHSV(0, 0, bright);
-//     delay(200);
-// }
+    if (btn2){
+        
+        funk=funk+255;
+        // 65535/255=257 diaforetika xrwmata!!!! 
+    }
+
+    if (funk==65535){
+        
+        funk=0;
+
+    }
+
+}
+
 
 void setup() {
     
     pixels.begin();
     pinMode (9, INPUT);
+    pinMode (10, INPUT);
 
 }
-
 
 void loop() {
 
     btn = digitalRead (9);
+    btn2 = digitalRead(10);
+    funkyColors();
     
     if (btn) {
 
-        brght=40;
+        brght=48;
 
         circlex3left();
 
@@ -116,9 +104,8 @@ void loop() {
         }
     }
     
-    brght=brght-4;
-    //delay(10);
-
+    brght=brght-3;
+    
     if (brght<0){
         
         brght=0;
