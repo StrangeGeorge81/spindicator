053632ec2243aa1548083c62ecad793b78ef9b23
StrangeGeorge81 <pandis.g8@gmail.com>
2020-12-17 21:13:01 +0200

    Corrections n Boot Sequence

--- spindicator_ws2812b/spindicator_ws2812b.ino
+++ spindicator_ws2812b/spindicator_ws2812b.ino
@@ -80,7 +80,7 @@ void switchbtn() {
     if (btn3 && swh==0){    
         swh = 1;
     }
-    if (btn && swh==1){
+    if (btn3 && swh==1){
         swh = 0;
     }
 }
@@ -106,27 +106,33 @@ void setup() {
     pinMode (11, INPUT);
 
     // BOOT Secuence!!!
-/*
-    int i=0;
-    while (i<=12){
-        for (i=1; i<=12; i++){            
-            uint32_t colour = pixels.Color(i*20, i*20, i*20);
 
-            pixels.setPixelColor (i, colour);
-            pixels.setBrightness(42);
-            pixels.show();
-            delay(200);
-        }
-    }
-    for (i=0; i<=3; i++){
+    for (int i=0; i<=11; i++){            
+        uint32_t colour = pixels.Color((i+1)*20, (i+1)*20, (i+1)*20);
+
+        pixels.setPixelColor (i, colour);
         pixels.setBrightness(42);
+        pixels.show();
+        delay(81);
+    }
+    for (int i=42; i<102; i=i+3){
+       pixels.setBrightness(i);
+       pixels.show();
+       delay(10); 
+    }
+    for (int i=0; i<=3; i++){
+        pixels.setBrightness(102);
+        pixels.show();
         delay(21);
-        pixels.setBrightness(5);
+        pixels.setBrightness(41);
+        pixels.show();
+        delay(10);
+
     }
     pixels.clear();
     
     // Might work,might not....
-    */
+    
 }
 
 void loop() {
@@ -144,9 +150,11 @@ void loop() {
             brght=48;
             circlex3left();
         }
-        brght=brght-3;            
-        if (brght<0){
-            brght=0;
+        else{
+            brght=brght-3;            
+            if (brght<0){
+                brght=0;
+            }
         }
     }
     if (swh==1){
@@ -154,10 +162,12 @@ void loop() {
             brght=48;
             circlex3right();
         }
-        brght=brght-3;            
-        if (brght<0){
-            brght=0;
+        else{
+            brght=brght-3;            
+            if (brght<0){
+                brght=0;
+            }
         }
     }
-    
+
 }
